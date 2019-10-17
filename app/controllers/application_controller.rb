03fb216364bf78c37a8ce1d216309bbe2de0e7f8
Randy Taylor <randyisbusy@gmail.com>
2019-10-17 18:32:02 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
-
+    configure do
+    enable :sessions
+      set :session_secret, "secret"
+    end
     set :views, Proc.new { File.join(root, "../views/") }
     
     get '/' do 
