739b2bb85f35ec603032f0e6f2afb96080d9d75e
Randy Taylor <randyisbusy@gmail.com>
2019-10-17 18:32:49 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,8 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
     
     get '/' do 
+       @session = session
+       binding.pry
       erb :'/super_hero'
       
     end
